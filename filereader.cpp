046b29a20ced4a1461f19c0b9dfb0252a26a5515
Easyfor314 <vandi.var@yandex.ru>
2019-07-26 01:27:53 +0300

    Add *.cpp for class "filereader"

---
+++ filereader.cpp
@@ -0,0 +1,205 @@
+
+    #include "filereader.h"
+    void    filereader::load_file(string file_name)
+    {
+    name_file_saved=file_name;
+    file_read.open(name_file_saved); 
+    if (!file_read.is_open())
+    {
+        cout << "Program terminating.\n";
+        exit(EXIT_FAILURE);
+    } 
+    file_read.close();
+    }
+    void    filereader::ShowFirstPage(){
+    OnLoadPage(0);
+    
+    }
+    void    filereader::cashing_file(){
+        file_read.open(name_file_saved); 
+        file_read.seekg(0);
+        file.clear();
+        cout<<"cashe file:"<<endl;
+                while(getline(file_read,page_buffer[page_counter_line]) )
+                {   
+                file+= page_buffer[page_counter_line]+'\n';
+                cout<< page_buffer[page_counter_line]+'\n';
+                }
+        file_read.close();
+    }
+
+    void    filereader::OnLoadPage(int found_number_page){
+        file_read.open(name_file_saved); 
+    cout<<"Load_page - "<<found_number_page<<endl;  
+    
+    int page_counter_line=0;
+    int number_page=0;
+    file_read.seekg(0);
+
+    while(getline(file_read,page_buffer[page_counter_line]) )
+    {   
+                
+        
+        page_counter_line++;
+        
+        if(page_counter_line==PAGE_SIZE)
+        {
+           
+            if (number_page==found_number_page)
+            {      
+                          
+                break;
+            }
+            number_page++;
+            page_counter_line=0;      
+         
+            ClearPage();        
+        }
+       
+         
+    }
+     number_page_buffer=found_number_page;
+     file_read.close();
+    ShowLoadPage(page_counter_line);
+    };
+
+
+    void    filereader::ShowLoadPage(int size_page){    
+          
+        for(int i=0;i<size_page;i++)
+        {
+            cout<<i<<"__"<<page_buffer[i]<<endl;
+        }  
+
+    }
+     void   filereader::ShowLoadPage(){    
+            
+        for(int i=0;i<PAGE_SIZE;i++)
+        {
+            cout<<i<<"__"<<page_buffer[i]<<endl;
+        }  
+
+    }
+    void    filereader::ClearPage(){
+     
+        for(int i=0;i<PAGE_SIZE;i++)
+        {
+           page_buffer[i]="";
+        }  
+
+    }
+    void    filereader::cashe_page( string cashe[]){
+        for(int i=0;i<PAGE_SIZE;i++)
+        {
+           cashe[i]= page_buffer[i];
+        }  
+       
+    }
+    void    filereader::save_file(string file_name)
+    {
+    
+    file_write.open(file_name);
+    for(int i=0;i<savedfile.length();i++)
+        {
+        
+            file_write<<savedfile[i];
+            //cout<<savedfile[i];
+        } 
+        savedfile.clear();
+        
+
+        cout<<"file_saved : "<<file_name<<endl;
+     for(int i=0;i<savedfile.length();i++)
+        {
+            savedfile[i]='\0';
+        }
+
+    file_write.close();
+        //Загрузка текущей страницы в кэш
+        /* string page_cashe[PAGE_SIZE];
+        cashe_page(page_cashe);
+        
+        for(int i=0;i<PAGE_SIZE;i++)
+        {
+            file_write<<page_cashe[i]<<endl;
+        }  */
+       
+    }
+    void    filereader::get_line(int number_line)
+    {
+        cout<< page_buffer[check_number(number_line)];
+        
+    }
+    int filereader::check_number(int number_line)
+    {
+        if (number_line<=PAGE_SIZE)
+        {
+            return number_line;            
+        }
+        else {      
+            int PageNumberLine=(number_line)/PAGE_SIZE;
+            OnLoadPage(PageNumberLine);
+            return number_line - (PAGE_SIZE*PageNumberLine);     
+        }
+
+    }
+    void    filereader::insert_line(int number_line_insert,string line_to_insert)
+    {
+        cashing_file();
+        int number_line=0;
+       
+        for(int i=0;i<file.length();i++){
+            savedfile+=file[i];
+            
+            if(file[i]=='\n'){
+                number_line++;
+                
+            if (number_line==(number_line_insert-1))
+            {
+                savedfile+=line_to_insert+'\n';
+                cout<<"Line is inserted"<<endl;
+            }
+            }
+
+        }
+         if(number_line<(number_line_insert-1)){
+              savedfile+=line_to_insert+'\n';
+              cout<<"Line is inserted"<<endl;
+         }
+         save_file(name_file_saved);
+
+    }
+    void    filereader::replace_line(int number_line_replace, string line_to_replace)
+    {
+       
+    delete_line(number_line_replace);
+    insert_line(number_line_replace,line_to_replace);
+        
+    }
+    void    filereader::delete_line(int number_line_delete)
+    {
+        cashing_file();
+         int number_line=0;
+       
+        for(int i=0;i<file.length();i++){
+            if(number_line!=number_line_delete-1)
+            {savedfile+=file[i];  
+            }
+            if(file[i]=='\n'){
+                number_line++;               
+            }
+        }
+        if(number_line<(number_line_delete-1)){
+            cout<<"Fail deleted"<<endl;
+        }
+        else{
+            cout<<"Line is deleted"<<endl;
+        }
+        save_file(name_file_saved);
+    }
+   
+    void    filereader::delete_file_stack(){
+        file_read.close();
+        file_write.close();
+    }
+
